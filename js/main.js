899fab36ac928d029671a1543a1fdf7823177973
seaflapp <martin.digitaletalenter@gmail.com>
2024-06-17 13:03:23 +0200

    footer og start js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+const randomeNummber = Math.random()
+
+const dicethrow = Math.floor(Math.random() * 6)
+
+console.log(dicethrow)
+
+const diceresults = ["you die", "you defeated the monster", "you ran away scared", "you missed the monster you dufus!" , "you tripped!"]
+
+const stringresult = diceresults[dicethrow]
+
+console.log(stringresult)
